bdaba179b2a240b8fac14760a3d0b099045acbc8
Alexandre Hary <alexandre.harry10@gmail.com>
2023-08-29 14:51:59 +0200

    validates done

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   has_many :options
+  validates :title, presence: true
+  validates :position, presence: true
+  validates :last, inclusion: { in: [true, false] }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :chatrooms
   has_many :offers
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :role, presence: true
 end
