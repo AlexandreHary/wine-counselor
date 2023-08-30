8b4d2c81eaa0f45232acba77bf911cc9da29933d
Joris CHENNA joris64310@yahoo.fr <joris64310@yahoo.fr>
2023-08-30 10:45:26 +0200

    bookings controller and views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,19 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:chatroom_id, :wine_id)
   end
 end
