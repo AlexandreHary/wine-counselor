de8e4a1b622765b76e41a4c79f4424c3138e15df
enola lukasiewicz <enola.lukasiewicz@kedgebs.com>
2023-08-30 14:51:29 +0200

    last modif before it bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Chatroom.destroy_all
+User.destroy_all
+
+puts 'creating'
+
+user_1 = User.create!(first_name: "Alex", last_name: "HARY", email: "alexandre@winecounselor.com", password: "azerty1")
+user_2 = User.create!(first_name: "Joris", last_name: "CHENNA", email: "joris@winecounselor.com", password: "azerty2")
+user_3 = User.create!(first_name: "Enola", last_name: "LUKASIEWICZ", email: "enola@winecounselor.com", password: "azerty3")
+
+chatroom = Chatroom.create!(user: user_1)
+chatroom = Chatroom.create!(user: user_2)
+chatroom = Chatroom.create!(user: user_3)
+
+question_1 = Question.create!(question: "1. Quelle occasion ?")
+question_2 = Question.create!(question: "2. Quel type de repas ? ")
+question_3 = Question.create!(question: "3. Quelle couleur de vin ? (couleur)")
+question_4 = Question.create!(question: "4. Quel gout ? - Leger / fort ? – fruité / tanique")
+question_5 = Question.create!(question: "5. Voulez vous des bouteilles de même cru ou des cru différents ?")
+question_6 = Question.create!(question: "6. Combien de personnes ?")
+question_7 = Question.create!(question: "7. france /appelation / indication / vin local, région?")
+question_8 = Question.create!(question: "8. Quel est votre budget total ? – applicable à toutes les bouteilles ? oui – non")
+question_9= Question.create!(question: "9. Quelle quantité ?")
+question_10 = Question.create!(question: "10. Temps de livraison ?")
+question_11 = Question.create!(question: "11. Vin reconnu, ou petit vigneron Attention vin bio ? origine – recoltant – peut importe ?")
+puts 'finished'
