72d7ca36ec713f6a3a505259a559676be86a48e9
enola lukasiewicz <enola.lukasiewicz@kedgebs.com>
2023-08-30 18:35:27 +0200

    card bien avancé pour la show des selections de wines

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,11 +1,15 @@
 class ChatroomsController < ApplicationController
+  def new
+    @chatroom = Chatroom.new
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
   end
 
   def create
     @chatroom = Chatroom.create!(user: current_user)
-    redirect_to chatroom_path(@chatroom), notice: "Bienvenue sur WineCounselor!", status: :see_other
+    redirect_to chatroom_path(@chatroom), notice: "Bienvenue sur WineCounselor !", status: :see_other
   end
 
   private
