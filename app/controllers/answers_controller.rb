32adc2154969e58a34a16f3bfe34cedda96155c8
Alexandre Hary <alexandre.harry10@gmail.com>
2023-08-30 15:09:27 +0200

    modif seed

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,12 @@
 class AnswersController < ApplicationController
-  def create
+   def create
+  #   @answer = current_user.answers.create(answer_params)
+  #   render json: { message: 'Answer saved successfully' }
   end
+
+  # private
+
+  # def answer_params
+  #   params.require(:answer).permit(:content)
+  # end
 end
