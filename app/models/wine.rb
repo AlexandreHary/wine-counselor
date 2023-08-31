191e30194611044b41c60f7a3212afb082fe6954
enola lukasiewicz <enola.lukasiewicz@kedgebs.com>
2023-08-31 17:06:03 +0200

    ajout des pictures et mise à jour de la seed

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,9 +2,13 @@ class Wine < ApplicationRecord
   has_many :bookings
   has_many :offers
   has_many :wine_tags
+
+  has_one_attached :wine_photo
+
   validates :rating, presence: true
   validates :name, presence: true
   validates :description, presence: true
   validates :year, presence: true
   validates :appellation, presence: true
+  #  validates :wine_photo, presence: true
 end
