e6d47794ca4ac727de1ce03d6b064584f0cbfc30
Joris CHENNA joris64310@yahoo.fr <joris64310@yahoo.fr>
2023-09-06 11:55:41 +0200

    show caviste

--- db/seeds/01_user.rb
+++ db/seeds/01_user.rb
@@ -3,4 +3,3 @@ puts 'creating users'
 user_1 = User.create!(first_name: "Alex", last_name: "HARY", email: "alexandre@winecounselor.com", password: "azerty1")
 user_2 = User.create!(first_name: "Joris", last_name: "CHENNA", email: "joris@winecounselor.com", password: "azerty2")
 user_3 = User.create!(first_name: "Enola", last_name: "LUKASIEWICZ", email: "enola@winecounselor.com", password: "azerty3")
-  
