3a8ab120b8d4edd5ea4b1ceff48f269ddbf1f8f0
Joris CHENNA joris64310@yahoo.fr <joris64310@yahoo.fr>
2023-09-07 10:05:33 +0200

    redirect caviste

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @chatroom = Chatroom.new
+    if signed_in? && current_user.caviste?
+      redirect_to caviste_offers_path
+    end
   end
 end
